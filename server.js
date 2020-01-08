69890d73794a8c563a5b663ef426cc94412736e0
margaiwangara <margaiwangara@gmail.com>
2020-01-09 00:55:28 +0300

    my first graphql app created and tested, works

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const { buildSchema } = require("graphql");
 
 // Express Init
 const app = express();
@@ -6,6 +8,29 @@ const app = express();
 // Middleware, BodyParser
 app.use(express.json());
 
+// Build GraphQL Schema
+const schema = buildSchema(`type Query, {
+  hello: String
+}`);
+
+// Root provides a resolver function for each endpoint
+const root = {
+  hello: () => {
+    return "Hello World";
+  }
+};
+
+// Create Route
+app.use(      
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    rootValue: root,
+    graphiql: true
+  })
+);
+
+// Port
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
